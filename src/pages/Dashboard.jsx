0d0a75c57be0564a61f8d66112bb3d399ebe524b
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-12-20 06:10:53 +0530

    dashboard hover in user

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
+import { useState } from 'react';
 import Sidebar from '../components/Sidebar'
 import './css/Dashboard.css'
-import { User, Trash2 } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { CircleUserRound, Trash2, User } from "lucide-react";
+import { NavLink, useNavigate } from "react-router-dom";
+
 
 const Dashboard = () => {
   const navigate = useNavigate();
-
-  const handleProfileClick = () => {
-    navigate("/profile");
-  };
-
+  const [plan, setPlan] = useState("free")
+  const [user, setUser] = useState(false)
 
   return (
     <>
       <Sidebar />
       <div className='selected-content'>
         <div className='dashboard-nav'>
-          <h1>Welcome, Ankit Kumar</h1>
-          <p>Plan: free</p>
-          <p>Created At: 12/10/2024, 10:39:46 PM</p>
+          <div className='relative cursor-pointer' onMouseOver={() => setUser(true)} onMouseLeave={() => setUser(false)}>
+            <NavLink to='/profile'><h1 className='flex items-center gap-2 text-xl font-semibold'><CircleUserRound size={32} />Welcome, Ankit Kumar</h1></NavLink>
+            {user ? <div className='rounded-md user-over absolute top-8 py-4  w-[100%] h-[200px] flex flex-col items-center' onMouseLeave={() => setUser(false)}>
+                <div><CircleUserRound size={50} color="#ffffff" /></div>
+                <div className='my-2'><h1 className='text-white'>Username</h1></div>
+                <div className='my-4'><button className='btn'>Logout</button></div>
+            </div> : ""}
+          </div>
+          <p className='font-semibold text-xl' onMouseOver={() => setPlan("Upgrade")} onMouseLeave={() => setPlan("Free")}>Plan: {plan}</p>
+          <p className='hidden md:block'>Created At: 12/10/2024, 10:39:46 PM</p>
         </div>
-        <div className="dashboard-email-list">
+        <div className="dashboard-email-list my-4">
 
           <div className='email-list'>
             <div className='email-list-heading'>
